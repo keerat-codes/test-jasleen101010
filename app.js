f74f012630c372fb928fd90747e0d55a49a8076e
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 20:41:04 +0530

    Updated App.js for email route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const emailRoute = require('./routes/email');
+app.use('/email', emailRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
